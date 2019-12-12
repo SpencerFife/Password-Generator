30abd635ca95bec27ce61b00a59cd7e3aca15a10
Spencer Fife <scottyfife17@gmail.com>
2019-12-11 16:43:41 -0800

    greened out alerts and vars for later use while I tinker with the javascript

--- passwordgen.js
+++ passwordgen.js
@@ -1,6 +1,34 @@
 //prompts the user to input length of password//
 var passwordPrompt = prompt('How many characters would you like your password to contain?');
 var passwordLength {
-    
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('Password length must be more than 8 characters')
+//alert('Password length must be less than 129 characters');
+//alert('Click OK to include special characters');
+//alert('Click OK to include numeric characters');
+//alert('Click OK to include lowercase characters');
+//alert('Click OK to include uppercase characters');
+
+
+//var upperChar = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+//var lowerChar = ['abcdefghijklmnopqrstuvwxyz'];
+//var numerals = ['1234567890'];
+//var specChar = ['!@#$%^&*()'];
 
